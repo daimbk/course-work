1a754ea437dc968e662791193dc030e07fe6c8ea
daimbk <daimbkhalid@gmail.com>
2023-09-16 19:04:42 +0500

    lab upload

---
+++ operating-systems/lab1/hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	int age = 22;
+	printf("Hello World, I am %d years old", age);
+	return 0;
+}
+
---
+++ operating-systems/lab1/task-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int height = 10, width = 12, depth = 8, volume;
+	
+	volume = height * width * depth;
+	printf("Volume: %d", volume);
+	return 0;
+}
+
